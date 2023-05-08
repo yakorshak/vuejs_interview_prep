5c4f28568e9b9197675ab78950777ee663cc500c
Andrii Korshak <yakorshak@gmail.com>
2023-05-08 14:42:04 -0400

    project setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import AppHeader from "./components/common/AppHeader.vue";
+</script>
+
+<template>
+  <AppHeader />
+  <RouterView />
+</template>
+
+<style scoped>
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
